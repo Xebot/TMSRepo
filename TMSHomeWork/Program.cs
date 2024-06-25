fc10db59aecfb7c7b04e407117c9046f515d3fac
Stepan Kucherenko <stepan.kucherenko@yandex.ru>
2024-06-25 21:08:01 +0300

    add text file

--- TMSHomeWork/Program.cs
+++ TMSHomeWork/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+using System.Buffers;
+
+var text = "1 2";
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello1, World!2");
