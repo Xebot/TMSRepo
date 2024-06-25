cb4d3e689a86287f59fd9c106f63bd6f8f7a3223
Dmitry <diman_chelyad@mail.ru>
2024-06-25 21:32:19 +0300

    Added new

--- TMSHomeWork/Program.cs
+++ TMSHomeWork/Program.cs
@@ -4,3 +4,5 @@ using System.Buffers;
 var text = "1 2";
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello1, World!2");
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
